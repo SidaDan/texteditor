bb2d46f26c794345f049e7c2ec18ef4a6156bfb0
SidaDan <fschultz@sinf.de>
2018-10-10 14:37:09 +0200

    Added textarea with scrollbar to edit text.

--- src/com/texteditor/UI/BaseGUI.java
+++ src/com/texteditor/UI/BaseGUI.java
@@ -1,14 +1,37 @@
 package com.texteditor.UI;
 
+import java.awt.BorderLayout;
+import java.awt.Font;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 
-public class BaseGUI {
-	public BaseGUI() {
-		init();
-	}
-	private void init() {
-		JFrame window = new JFrame("Text Editor");
-		window.setSize(700, 800);
-		window.setVisible(true);
-	}
+public class BaseGUI extends JFrame {
+
+    JTextArea textArea;
+
+    public BaseGUI() {
+        init();
+    }
+
+    private void init() {
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        this.setSize(700, 800);
+        this.setTitle("Text Editor");
+        textArea = new JTextArea("", 0, 0);
+        textArea.setFont(new Font("Century Gothic", Font.PLAIN, 12));
+        textArea.setTabSize(2);
+        textArea.setFont(new Font("Century Gothic", Font.PLAIN, 12));
+        textArea.setTabSize(2);
+        textArea.setLineWrap(true);
+        JScrollPane scrollPane = new JScrollPane(textArea);
+        textArea.setWrapStyleWord(true);
+        scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+        getContentPane().setLayout(new BorderLayout()); 
+        JPanel panel = new JPanel(new BorderLayout());
+        panel.add(scrollPane);
+        getContentPane().add(panel);
+        this.setVisible(true);
+    }
 }
