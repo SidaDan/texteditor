f80fc1f817d98af042823ac435dc6cad0b7a278a
SidaDan <fschultz@sinf.de>
2018-10-12 09:33:12 +0200

    Prepared menu bar

--- src/com/texteditor/UI/BaseGUI.java
+++ src/com/texteditor/UI/BaseGUI.java
@@ -3,6 +3,8 @@ package com.texteditor.UI;
 import java.awt.BorderLayout;
 import java.awt.Font;
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
@@ -10,6 +12,8 @@ import javax.swing.JTextArea;
 public class BaseGUI extends JFrame {
 
     JTextArea textArea;
+    private  JMenuBar menuBar;
+    private  JMenu menuFile, menuEdit, menuAbout;
 
     public BaseGUI() {
         init();
@@ -17,6 +21,15 @@ public class BaseGUI extends JFrame {
 
     private void init() {
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        
+        menuFile = new JMenu("File");
+        menuEdit = new JMenu("Edit");
+        menuAbout = new JMenu("About");
+        menuBar = new JMenuBar();
+        menuBar.add(menuFile);
+        menuBar.add(menuEdit);
+        menuBar.add(menuAbout);
+        this.setJMenuBar(menuBar);
         this.setSize(700, 800);
         this.setTitle("Text Editor");
         textArea = new JTextArea("", 0, 0);
@@ -32,6 +45,8 @@ public class BaseGUI extends JFrame {
         JPanel panel = new JPanel(new BorderLayout());
         panel.add(scrollPane);
         getContentPane().add(panel);
+        
+        
         this.setVisible(true);
     }
 }
